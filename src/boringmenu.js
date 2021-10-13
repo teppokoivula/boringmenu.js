4d15a757f612b5a94ea58fb8a03afdfb6a387ff1
Teppo Koivula <teppo.koivula@avoine.fi>
2021-10-13 21:36:58 +0300

    Parcel 2, support for JS parent/active classes, and other updates

--- src/boringmenu.js
+++ src/boringmenu.js
@@ -3,27 +3,29 @@
 /**
  * boringmenu.js
  *
- * @version 0.0.5
+ * @version 0.1.0
  */
 export default class boringmenu {
 
 	/**
 	 * Constructor
 	 *
-   	 * @param {Object} options Options for an instance of boringmenu
+	 * @param {Object} options Options for an instance of boringmenu
 	 */
 	constructor(options = {}) {
 
 		// Default options
-		this.options = {
+		this.defaultOptions = {
 			selectors: {
 				menu: '.boringmenu',
-				menuItem: ':scope > li',
+				item: ':scope > li',
 			},
 			classes: {
-				menuItemActive: 'boringmenu__item--active',
-				menuToggle: 'boringmenu__toggle',
-				menuToggleTextContainer: 'boringmenu__sr-only',
+				item: 'boringmenu__item',
+				itemActive: 'boringmenu__item--active',
+				itemParent: 'boringmenu__item--parent',
+				toggle: 'boringmenu__toggle',
+				toggleTextContainer: 'boringmenu__sr-only',
 			},
 			labels: {
 				'menu.open': 'Open',
@@ -35,6 +37,9 @@ export default class boringmenu {
 			},
 			id: 'boringmenu-' + this.getID(),
 		};
+
+		// Polyfills for IE11
+		this.polyfills();
 
 		// Merge our provided options with defaults
 		this.options = this.mergeOptions(options);
@@ -50,22 +55,15 @@ export default class boringmenu {
 		// Running counter for menu num
 		this.menuNum = 0;
 
-		// Polyfill NodeList.forEach (IE11)
-		if ('NodeList' in window && !NodeList.prototype.forEach) {
-			NodeList.prototype.forEach = function (callback, thisArg) {
-				thisArg = thisArg || window;
-				for (var i = 0; i < this.length; i++) {
-					callback.call(thisArg, this[i], i, this);
-				}
-			};
-		}
-
 		// Merge menu options with existing options
 		if (this.menu.getAttribute('data-boringmenu')) {
 			try {
-				this.options = this.mergeOptions(JSON.parse(this.menu.getAttribute('data-boringmenu')));
+				this.options = this.mergeOptions(JSON.parse(this.menu.getAttribute('data-boringmenu'), this.options));
 			} catch(e) {};
 		}
+
+		// Add active and parent classes
+		this.addClasses(this.menu, this.options.classes);
 
 		// Find submenu elements and create toggles
 		this.findSubMenus(this.menu);
@@ -78,15 +76,40 @@ export default class boringmenu {
 	}
 
 	/**
+	 * Add active and parent classes
+	 *
+	 * @param {Object} menu
+	 * @param {Object} classes
+	 */
+	addClasses(menu, classes) {
+		const currentPath = window.location.pathname.replace(/\/$/, '') + '/';
+		menu.querySelectorAll('.' + classes.item + '[href="' + currentPath + '"]').forEach(item => {
+			item.classList.add(classes.itemActive);
+			item = item.parentNode;
+			while (item.parentNode) {
+				item = item.parentNode;
+				if (item.tagName == 'LI') {
+					const childItem = item.firstChild;
+					if (childItem !== null && childItem.classList.contains(classes.item) && !childItem.classList.contains(classes.itemActive)) {
+						childItem.classList.add(classes.itemParent);
+					}
+				} else if (item.matches(this.options.selectors.menu)) {
+					break;
+				}
+			}
+		});
+	}
+
+	/**
 	 * Find submenu elements and create toggles
 	 *
-	 * @param {Object} menu Menu element
+	 * @param {Object} menu
 	 */
 	findSubMenus(menu) {
-		menu.querySelectorAll(this.options.selectors.menuItem).forEach((menuItem) => {
+		menu.querySelectorAll(this.options.selectors.item).forEach((item) => {
 
 			// Look for a submenu, bail out early if none found
-			const submenu = menuItem.querySelector('ul');
+			const submenu = item.querySelector('ul');
 			if (!submenu) return;
 
 			// Add unique ID
@@ -94,12 +117,12 @@ export default class boringmenu {
 			submenu.setAttribute('id', this.options.id + '-' + this.menuNum);
 
 			// Hide menu
-			if (!menuItem.classList.contains(this.options.classes.menuItemActive) && !submenu.querySelectorAll('.' + this.options.classes.menuItemActive).length) {
+			if (!item.classList.contains(this.options.classes.itemActive) && !item.querySelector('.' + this.options.classes.itemActive)) {
 				submenu.hidden = true;
 			}
 
 			// Insert toggle button before menu item
-			submenu.parentNode.insertBefore(this.getMenuToggle(submenu), submenu);
+			submenu.parentNode.insertBefore(this.getToggle(submenu), submenu);
 
 			// Find nested submenu elements
 			this.findSubMenus(submenu);
@@ -121,7 +144,7 @@ export default class boringmenu {
 		menuToggle.setAttribute('aria-expanded', !menu.hidden);
 		menuToggleText.nodeValue = this.options.labels[menu.hidden ? 'menu.open' : 'menu.close'];
 		if (menuToggleIcon != null) {
-			const newMenuToggleIcon = this.getMenuToggleIcon(menu, menuToggleIcon);
+			const newMenuToggleIcon = this.getToggleIcon(menu, menuToggleIcon);
 			menuToggleIcon.parentNode.replaceChild(newMenuToggleIcon, menuToggleIcon);
 			menuToggleIcon = newMenuToggleIcon;
 		}
@@ -136,26 +159,26 @@ export default class boringmenu {
 	 * @param {Object} submenu
 	 * @returns {Object}
 	 */
-	getMenuToggle(submenu) {
+	getToggle(submenu) {
 
 		// Create menu toggle button
 		const menuToggle = document.createElement('button');
-		menuToggle.classList.add(this.options.classes.menuToggle);
+		menuToggle.classList.add(this.options.classes.toggle);
 		menuToggle.setAttribute('aria-haspopup', 'true');
 		menuToggle.setAttribute('aria-expanded', !submenu.hidden);
 		menuToggle.setAttribute('aria-controls', submenu.getAttribute('id'));
 
 		// Add text within container
 		const menuToggleTextContainer = document.createElement('span');
-		if (this.options.classes.menuToggleTextContainer) {
-			menuToggleTextContainer.classList.add(this.options.classes.menuToggleTextContainer);
+		if (this.options.classes.toggleTextContainer) {
+			menuToggleTextContainer.classList.add(this.options.classes.toggleTextContainer);
 		}
 		menuToggle.appendChild(menuToggleTextContainer);
 		const menuToggleText = document.createTextNode(this.options.labels[submenu.hidden ? 'menu.open' : 'menu.close']);
 		menuToggleTextContainer.appendChild(menuToggleText);
 
 		// Add icon (optional)
-		let menuToggleIcon = this.getMenuToggleIcon(submenu);
+		let menuToggleIcon = this.getToggleIcon(submenu);
 		if (menuToggleIcon != null) {
 			menuToggle.appendChild(menuToggleIcon);
 		}
@@ -193,7 +216,7 @@ export default class boringmenu {
 	 * @param {(Object|null)} menuToggleIcon
 	 * @returns {(Object|null)}
 	 */
-	getMenuToggleIcon(submenu, menuToggleIcon) {
+	getToggleIcon(submenu, menuToggleIcon) {
 		menuToggleIcon = menuToggleIcon || null;
 		if (this.options.icons['menu.open'] && this.options.icons['menu.close']) {
 			menuToggleIcon = this.options.icons[submenu.hidden ? 'menu.open' : 'menu.close'];
@@ -204,13 +227,17 @@ export default class boringmenu {
 				menuToggleIconClass.split(' ').forEach((iconClass) => {
 					menuToggleIcon.classList.add(iconClass);
 				});
+			} else if (typeof menuToggleIcon == 'object' && typeof menuToggleIcon.cloneNode == 'function') {
+				menuToggleIcon = menuToggleIcon.cloneNode(true);
+			} else if (typeof menuToggleIcon == 'function') {
+				menuToggleIcon = menuToggleIcon();
 			}
 		}
 		return menuToggleIcon;
 	}
 
 	/**
-	 * Merge array of new options with current options
+	 * Merge array of new options with default options
 	 *
 	 * @param {Object} options
 	 * @param {(Object|null)} defaults
@@ -218,10 +245,15 @@ export default class boringmenu {
 	 */
 	mergeOptions(options, defaults) {
 		if (defaults == null) {
-			defaults = this.options;
+			defaults = this.defaultOptions;
 		}
-		for (const key of Object.keys(options)) {
-			if (typeof options[key] === 'object') {
+		let keys = Object.keys(options);
+		if (!keys.length) {
+			return defaults;
+		}
+		for (let i = 0; i < keys.length; i++) {
+			const key = keys[i];
+			if (typeof defaults[key] === 'object') {
 				Object.assign(options[key], this.mergeOptions(defaults[key], options[key]));
 			}
 		}
@@ -238,4 +270,51 @@ export default class boringmenu {
 		return Date.now();
 	}
 
+	/**
+	 * Initialize polyfills
+	 */
+	polyfills() {
+
+		// Polyfill NodeList.forEach (IE11)
+		// https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
+		if ('NodeList' in window && !NodeList.prototype.forEach) {
+			NodeList.prototype.forEach = function (callback, thisArg) {
+				thisArg = thisArg || window;
+				for (let i = 0; i < this.length; i++) {
+					callback.call(thisArg, this[i], i, this);
+				}
+			};
+		}
+
+		// Polyfill Object.assign (IE11)
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+		if (typeof Object.assign !== 'function') {
+			// Must be writable: true, enumerable: false, configurable: true
+			Object.defineProperty(Object, "assign", {
+				value: function assign(target, varArgs) { // .length of function is 2
+					if (target === null || target === undefined) {
+						throw new TypeError('Cannot convert undefined or null to object');
+					}
+
+					let to = Object(target);
+
+					for (let i = 1; i < arguments.length; i++) {
+						let nextSource = arguments[i];
+
+						if (nextSource !== null && nextSource !== undefined) {
+							for (let nextKey in nextSource) {
+								// Avoid bugs when hasOwnProperty is shadowed
+								if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+									to[nextKey] = nextSource[nextKey];
+								}
+							}
+						}
+					}
+					return to;
+				},
+				writable: true,
+				configurable: true
+			});
+		}
+	}
 }
